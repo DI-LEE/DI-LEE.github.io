9963f4b874e22efc105241aa2c657a3926c2558b
DI-LEE <79498819+DI-LEE@users.noreply.github.com>
2025-08-04 04:56:52 +0000

    Deploying to gh-pages from @ DI-LEE/DI-LEE.github.io@3daf49c9a157e19338dff28c21c3b38a15a1ba6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -133,13 +133,6 @@ ninja.data = [{
           window.open("https://github.com/DI-LEE", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
@@ -147,18 +140,18 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/dong-in-lee", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=VRcajyUAAAAJ&hl", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/DongInLeeD51567", "_blank");
         },
       },{
       id: 'light-theme',
